0e08d1f84f478c74cb18a8f24295545d6050b9fc
Ka-Na03 <kaito1129daku@gmail.com>
2023-09-02 05:17:23 +0900

    flashで表示するメッセージの編集

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
       session[:user_id] = @user.id
       redirect_to root_path, notice: "ログインしました"
     else
-      flash.now[:alert] = "ログインできませんでした"
+      flash.now[:alert] = "ログインできませんでした。<br>入力した内容に誤りがあります。"
       render :new
     end
   end
